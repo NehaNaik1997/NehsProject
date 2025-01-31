72a8d20fcc5bf2a711805cb961390a7da000fdc9
NehaNaik <nehavinodnaik97@gmail.com>
2025-01-31 18:54:41 +0530

    resolve mearge conflict

--- src/test/java/com/actitime_automation/BrowseLanchingProgram.java
+++ src/test/java/com/actitime_automation/BrowseLanchingProgram.java
@@ -7,6 +7,10 @@ public class BrowseLanchingProgram {
     {
         GenericUtility gu = new GenericUtility();
         gu.startUp("ch", "https://www.flipkart.com/");
+<<<<<<< HEAD
         System.out.println("Doing changes and Try to push git hub ");
+=======
+        System.out.println("changes doing by neha");
+>>>>>>> 5e20b43 (lanch browser changes doing by neha)
     }
 }
